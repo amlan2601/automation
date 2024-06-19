45462b3cef0495e489a3d188aae526cd4b017fdf
amlan2601 <md.amlan@technonext.com>
2024-06-19 14:00:32 +0600

    index file change

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+echo "console.log('Hello, World!');"
 root.render(
   <React.StrictMode>
     <App />
